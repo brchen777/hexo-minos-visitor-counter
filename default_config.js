36a1454e36cdfcc9943d379a50b6586e895a7fb6
brchen777 <eric790620@gmail.com>
2019-11-06 23:09:35 +0800

    Add default config and check dependency

---
+++ default_config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    "firebase-version": "7.2.3",
+    apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    authDomain: "wwww.example.com",
+    databaseURL: "https://example.firebaseio.com"
+};
--- index.js
+++ index.js
@@ -1,22 +1,53 @@
 const fs = require("fs");
+const logger = require("hexo-log")();
 const uglifyJS = require("uglify-es");
+const defaultConfig = require("./default_config");
 
-const { "firebase-version": version = "7.2.3", ...configArgs } = hexo.config["visitor-counter"];
+logger.info(`=======================================
+HEXO MINOS Visitor Counter
+=============================================`);
 
+// check dependency
+logger.info("Checking dependencies");
+const missingDeps = ["hexo-inject"]
+    .map(name => {
+        try {
+            require.resolve(name);
+            return true;
+        } catch (e) {
+            logger.error(`Package ${name} is not installed.`);
+        }
+        return false;
+    })
+    .some(installed => !installed);
+if (missingDeps) {
+    logger.error(
+        "Please install the missing dependencies in the root directory of your Hexo site."
+    );
+    process.exit(-1);
+}
+
+const config = Object.assign(Object.seal({ ...defaultConfig }), hexo.config["visitor-counter"]);
+const { "firebase-version": version, ...otherConfig } = config;
+let jsContent = fs.readFileSync(`${__dirname}/lib/visitor-counter.js`, "utf8");
+
+// replace firebase config
+for (const key in otherConfig) {
+    if (!otherConfig.hasOwnProperty(key)) continue;
+
+    const val = otherConfig[key];
+    const re = new RegExp(`__${key}__`, "g");
+    jsContent = jsContent.replace(re, val);
+}
+
+// minify js content
+const { code: minJsContent } = uglifyJS.minify(jsContent);
+
+// inject script
 hexo.extend.filter.register("inject_ready", inject => {
-    // inject dependencies
     const cdnUrl = "https://www.gstatic.com/firebasejs";
-    inject.bodyEnd.script({ src: `${cdnUrl}/${version}/firebase-app.js` });
-    inject.bodyEnd.script({ src: `${cdnUrl}/${version}/firebase-database.js` });
-
-    let jsContent = fs.readFileSync(`${__dirname}/lib/visitor-counter.js`, "utf8");
-    // replace config
-    for (const key in configArgs) {
-        const val = configArgs[key];
-        const re = new RegExp(`__${key}__`, "g");
-        jsContent = jsContent.replace(re, val);
-    }
-    // minify js content
-    const { code: minJsContent } = uglifyJS.minify(jsContent);
-    inject.bodyEnd.script({}, minJsContent);
+    inject.bodyEnd
+        .script({ src: `${cdnUrl}/${version}/firebase-app.js` })
+        .script({ src: `${cdnUrl}/${version}/firebase-database.js` })
+        .script({}, minJsContent);
 });
